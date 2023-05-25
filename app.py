c4c26432ac48ece6ec29cd9befb4d5277f707820
lee88jl <lee88jl@gmail.com>
2023-05-25 18:53:03 -0400

    Final

--- app.py
+++ app.py
@@ -1,52 +1,46 @@
-'''
-This is a sample flask application demoing how model 
-modularization improves the ability to use and deploy 
-trained models.
-'''
-
 # Importing dependencies
-from flask import Flask
+from flask import Flask, render_template, request
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 
 # Reading in file
-file = pd.read_csv('games.csv')
+file = pd.read_csv('./Resources/games_scaled.csv')
+file1 = pd.read_csv('./Resources/games.csv')
 
 # Creating Flask application
 app = Flask(__name__)
 
 # Creating the home route
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def home():
-    return "<h1>Hello World!</h1>"
+    if request.method == "POST":
+        search_query = request.form.get("search_query")
+        neigh = NearestNeighbors(n_neighbors=6)
+        neigh.fit(file.iloc[:, 3:39])
 
-# Creating an api route usin the model
-@app.route("/api/<X>")
+        # Perform a search based on the given query
+        search_results = file[file["Name"].str.contains(search_query, case=False)]
 
-def api(X):
+        # If no search results are found, return an appropriate message
+        if search_results.empty:
+            return "No matching results found."
+
+        game = search_results.iloc[0, 3:39]
+
+        closest_neighbor = neigh.kneighbors(game.to_numpy().reshape(1, -1))
+
+        columns_to_return = [0, 1, 2, 3, 4, 5, 40, 41, 42, 43, 44, 45, 46, 47]
         
-    neigh = NearestNeighbors(n_neighbors=3)
-    neigh.fit(file.iloc[:,3:10])
-    
-    game = file.iloc[int(X)][3:10]
-    print()
-    print()
-    print()
-    print()
-    print(X)
-    print()
-    print()
-    print()
-    print()
-    closest_neighbor = neigh.kneighbors(game.to_numpy().reshape(1, -1))
-    
-    s = file.iloc[closest_neighbor[1][0][1],:3]
-    df = pd.DataFrame(s)
-    
-    
-    # Return the prediction to the user
-    return df.to_html()
-    
+        i = closest_neighbor[1][0]
+
+        selected_columns = file1.iloc[i, columns_to_return]
+
+        # Render the HTML template and pass the selected columns as a parameter
+        return render_template("index.html", result=selected_columns.to_html())
+    else:
+        # Render the HTML template and pass the selected columns as a parameter
+        return render_template("index.html", result="")
+
 # Running the app
 if __name__ == '__main__':
     app.run(debug=True)
